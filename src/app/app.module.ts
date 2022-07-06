9aba35416fc5e28bbffddf16cc59eae449c213e4
tes7a <ttes7a@gmail.com>
2022-07-06 17:00:17 +0300

    create components and simple auth from

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { PortalModule } from '@angular/cdk/portal';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { FormBuilderComponent } from './form-builder/form-builder.component';
+import { DragSectionComponent } from './drag-section/drag-section.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    FormBuilderComponent,
+    DragSectionComponent,
+    AuthComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PortalModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    CdkAccordionModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
