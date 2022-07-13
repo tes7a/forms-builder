e52693c56fb12d6e82e423ace09ce8348b944e9b
tes7a <ttes7a@gmail.com>
2022-07-13 19:54:33 +0300

    add ngrx in proj, create store and reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
 import { FormBuilderComponent } from './form-builder/form-builder.component';
@@ -19,6 +23,9 @@ import { DragSectionComponent } from './drag-section/drag-section.component';
 import { AuthComponent } from './auth/auth.component';
 import { RoutingModule } from './routing.module';
 import { BuilderComponent } from './builder/builder.component';
+import { reducers, metaReducers } from './reducers';
+import { environment } from '../environments/environment';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
@@ -44,6 +51,12 @@ import { BuilderComponent } from './builder/builder.component';
     MatInputModule,
     MatIconModule,
     MatFormFieldModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+    EffectsModule.forRoot([AppEffects]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/reducers/elemets-reducers.ts
@@ -0,0 +1,9 @@
+const initialState = {
+  someArr: [],
+};
+
+export const ElementReducers = (state = initialState, actions: any): void => {
+  switch (actions.type) {
+    default: return state;
+  }
+};
